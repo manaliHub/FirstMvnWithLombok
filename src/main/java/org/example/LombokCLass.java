9d5f7872f1a9d323f0db86ddf05645615abd9831
moholkar.manali@gmail.com <moholkar.manali@gmail.com>
2024-04-07 16:49:48 +1100

    first commit

---
+++ src/main/java/org/example/LombokCLass.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import lombok.Data;
+
+@Data
+public class LombokCLass {
+    public String name;
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        LombokCLass lClass = new LombokCLass();
+        // lClass.getClass()
+        lClass.setName("Manali1");
+        System.out.println("Hello and welcome! "+ lClass.getName());
+    }
+}
